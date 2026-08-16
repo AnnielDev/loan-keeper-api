c79ca600a07af3c84aa020d37fdb190355ba9f28
Anniel <annielfranciscoreyes@gmail.com>
2026-08-15 21:58:59 -0400

    feat: add public decorator to the index route for access control

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './modules/auth/decorators/public.decorator';
 
 @Controller({})
 export class AppController {
+  @Public()
   @Get('/')
   index() {
     return '😎 Welcome to Loan Keeper API 😎';
